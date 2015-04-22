92cbe15f2cb74020bd4613b1aab78e54faa8472a
Luca Chiodini <luca@chiodini.org>
2015-04-22 10:49:26 +0200

    Fix: use UPPER_CASE for constants

--- download.py
+++ download.py
@@ -34,8 +34,8 @@ OUTPUT_FILENAME = "output.txt"
 
 # Try to perform an OAuth with Twitter.
 # TODO Catch exceptions.
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 api = tweepy.API(auth)
 
